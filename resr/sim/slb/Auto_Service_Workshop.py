51f001d9f66a4b6c9fd5e20af5736963ecb1aaec
Murat Anlı <52565584+omanli@users.noreply.github.com>
2024-05-29 03:03:11 +0300

    Update Auto_Service_Workshop.py

--- resr/sim/slb/Auto_Service_Workshop.py
+++ resr/sim/slb/Auto_Service_Workshop.py
@@ -1,5 +1,5 @@
 """ Auto Service Workshop
-    Jobs from multiple classes arrive with exponential interarrival times
+    Jobs from multiple classes arrive with exp. i-arrival times
     Resources are Lifts, Sr_Mechanics, and Jr_Mechanics
     Jobs have class dependent sequence of tasks
     Jobs have class dependent priorities in seizing resources
@@ -19,7 +19,7 @@ D = ASW.Read_Instance('Model_v1.yaml')
 TODO:
 -- Sim Output write to File
 -- Implement Replications
--- Implement Warm Up Period
+[] Implement Warm Up Period
 [] Report Utilization Stats
 [] Report WIP Stats
 [] Report Waiting Time Stats
@@ -27,28 +27,16 @@ TODO:
 TODO:
   - If more than one type of resource is available to a Request 
        decide which one to allocate
-  - Among multiple Requests that can be honored select which ones 
+  - Among multiple Requests that can be honored select which one(s) 
        to respond to
-
-  - not a good idea: using .standby() in the Scheduler
-    rather than activating the Scheduler from other components
-    creates extra calls to the scheduler when the scheduler
-    acts on behalf of other components
-       https://www.salabim.org/manual/Modelling.html
-  - use stores/queues to model requests for servers/resources
-       Requests as sim.Queue()
-       https://www.salabim.org/manual/Modelling.html
-       https://www.salabim.org/manual/Store.html
-  - or use a Queue to wait for a servers (multiple classes (one of the other) at the same time)
-       use stores to maintain instances of classes of servers
-       or make model servers as sim.Resource()
-  - figure out different service times when assigned to 
-    different classes of servers
 """
 
 import salabim as sim
-from Util import RV, TU, SNS, Enum, dirm
+from .Util import RV, TU, SNS, Enum, dirm
 import yaml
+
+inf = float('inf')
+
 
 SIM  = SNS()
 SHOP = SNS()
@@ -57,7 +45,8 @@ JOB  = SNS()
 
 SIM.time_unit = 'hours'
 SIM.trace = False
-SIM.env = sim.Environment(time_unit=SIM.time_unit, trace=SIM.trace)
+SIM.env = sim.Environment(time_unit=SIM.time_unit, 
+                          trace=SIM.trace)
 
 SIM.print_log = False
 #
@@ -115,7 +104,8 @@ def print_shop_state():
 
 class Arrival_Process(sim.Component):
     def __init__(self, job_type, *args, **kwargs):
-        super().__init__(name=SIM.fmt_apn.format(job_type), *args, **kwargs)
+        super().__init__(name=SIM.fmt_apn.format(job_type), 
+                         *args, **kwargs)
         self.job_type = job_type
 
     def process(self):
@@ -133,12 +123,13 @@ class Arrival_Process(sim.Component):
 
 class Job(sim.Component):
     def __init__(self, job_type, idx, *args, **kwargs):
-        super().__init__(name=SIM.fmt_jid.format(job_type, idx), *args, **kwargs)
+        super().__init__(name=SIM.fmt_jid.format(job_type, idx), 
+                         *args, **kwargs)
         self.idx      = idx
         self.job_type = job_type
         self.job_prty = JOB.priority[job_type]
         self.t_arr    = SIM.env.now()
-        self.t_fin    = None
+        self.t_dep    = inf
         self.WT       = []
         self.rsgr_req = None
         self.resr_asg = None
@@ -158,7 +149,7 @@ class Job(sim.Component):
             self.task.append(SNS())
             self.task[-1].job_type = T.type
             self.task[-1].res_grp  = T.rsgr
-            self.WT.append(0)
+            self.WT.append(inf)
             
             print_job_state(self, f"{T.name:19} [{T.rsgr}]")
 
@@ -166,7 +157,8 @@ class Job(sim.Component):
             if 'S' in T.type:
                 self.rsgr_req = T.rsgr
                 self.task[-1].t_s_req = SIM.env.now()
-                SIM.Requests.add_sorted(component=self, priority=self.job_prty)
+                SIM.Requests.add_sorted(component=self, 
+                                        priority=self.job_prty)
                 t_req = SIM.env.now()
                 SIM.Scheduler.activate()
                 self.passivate()
@@ -201,7 +193,7 @@ class Job(sim.Component):
                 assert self.resr_asg is None
                 SIM.Scheduler.activate()
 
-        self.t_fin = SIM.env.now()
+        self.t_dep = SIM.env.now()
         SIM.System.remove(self)
 
 
@@ -216,7 +208,6 @@ class Scheduler(sim.Component):
                 print_time_now()
                 print(" [Schdl] Activates")
             for j in SIM.Requests:
-                # print(f"    {j.name()} " + (j.rsgr_req if j.rsgr_req is not None else "--"))
                 for r in SHOP.ResourceGroups[j.rsgr_req]:
                     if SIM.Resource[r].available_quantity() > 0:
                         SIM.Requests.remove(j)
@@ -225,21 +216,6 @@ class Scheduler(sim.Component):
                         j.request(SIM.Resource[r])
                         break
             self.passivate()
-            """
-            if (len(Reqs) > 0) and ():
-                print(f"{rg:>13} Req:" + \
-                  " ".join(f"{sum(1 for j in Reqs 
-                             if j.job_type == jt):2}" 
-                             for jt in JOB.types), end=" ")
-                print(" Avl:" + \
-                  " ".join(f"{SIM.Resource[Res].available_quantity():2}" 
-                           for Res in SHOP.ResourceGroups[rg]))
-                print(SIM.Resource)
-            while (len(SIM.TBS) == 0) or all(not s.ispassive() for s in SIM.Servers.values()):
-                self.passivate()
-            s = [s for s in SIM.Servers.values() if s.ispassive()][0]
-            j = Req.pop()
-            """
 
 
 
@@ -250,8 +226,9 @@ def Read_Instance(fn):
         D = yaml.safe_load(yf.read())
 
     sim_fields = """
-        time_unit 
-        sim_length 
+        time_unit
+        sim_length
+        wup_length
         random_seed
         trace
     """.split()
@@ -265,7 +242,8 @@ def Read_Instance(fn):
     IAT = {}
     for typ,par in JOB.ia_time.items():
         p = par.split()
-        IAT[typ] = RV(SIM.env, p[0], p[1], int(p[2]), *(float(x) for x in p[3:]))
+        IAT[typ] = RV(SIM.env, p[0], p[1], int(p[2]), 
+                      *(float(x) for x in p[3:]))
     # JOB.ia_time = SNS(**IAT)
     # JOB.priority = SNS(**JOB.priority)
     JOB.ia_time = IAT
@@ -276,7 +254,8 @@ def Read_Instance(fn):
             if isinstance(X.dur, dict):
                 for r,par in X.dur.items():
                     p = par.split()
-                    X.dur[r] = RV(SIM.env, p[0], p[1], int(p[2]), *(float(x) for x in p[3:]))
+                    X.dur[r] = RV(SIM.env, p[0], p[1], int(p[2]), 
+                                  *(float(x) for x in p[3:]))
             if X.dur == "None":
                 X.dur = None
 
@@ -285,9 +264,11 @@ def Read_Instance(fn):
     for T in SHOP.ResourceGroups.keys():
         SHOP.ResourceGroups[T] = SHOP.ResourceGroups[T].split()
 
-    SHOP.Resources = [R for T in SHOP.ResourceTypes.values() for R in T]
+    SHOP.Resources = [R for T in SHOP.ResourceTypes.values() 
+                        for R in T]
 
-    SHOP.rtyp = {r:t for t,R in SHOP.ResourceTypes.items() for r in R}
+    SHOP.rtyp = {r:t for t,R in SHOP.ResourceTypes.items() 
+                     for r in R}
 
     return
 
@@ -299,16 +280,21 @@ def Run_Instance():
 
     # convert sim_length to env default time unit
     T = getattr(SIM.env, SIM.time_unit)(SIM.sim_length)
+    W = getattr(SIM.env, SIM.time_unit)(SIM.wup_length)
 
-    Run(SIM.random_seed, T)
+    Run(rs=SIM.random_seed, T=T, W=W)
+    m = Calculate_Metrics(T=T, W=W)
+    Print_Metrics(m)
 
 
 
-def Run(rs, T):
+def Run(rs, T, W=0):
     if rs is None:
         rs = SIM.random_seed
 
-    SIM.env = sim.Environment(time_unit=SIM.time_unit, trace=SIM.trace, do_reset=True)
+    SIM.env = sim.Environment(time_unit=SIM.time_unit, 
+                              trace=SIM.trace, 
+                              do_reset=True)
 
     if rs is not None:
         SIM.env.random_seed(rs)
@@ -340,61 +326,93 @@ def Run(rs, T):
     SIM.Arrivals = []
     SIM.n_arrivals = { p:0 for p in JOB.types }
 
+    if W > 0:
+        SIM.env.run(till=W)
+        [m.reset() for R in SIM.Resource.values() for m in R.all_monitors()]
+        [m.reset() for m in SIM.System.all_monitors()]
+        [m.reset() for m in SIM.Requests.all_monitors()]
+
     SIM.env.run(till=T)
 
+    return
 
 
-    print()
-    print((f"{'Type':>12s} {'#Arr':>6s} {'#Cmp':>6s} {'#WIP':>6s} "
+def Print_Metrics(m):
+    print(("\n" f"{'Type':>12s} {'#Arr':>6s} {'#Cmp':>6s} {'#WIP':>6s} "
            f"{'AST':>6} {'AWT':>6} {'ATT':>6} {'NiS':>6}"))
-    AWR = { typ:{r:0 for r in SHOP.ResourceGroups.keys()} for typ in JOB.types}
-    for typ in JOB.types:
-        na = SIM.n_arrivals[typ]
-        nc = sum(1 for j in SIM.Arrivals if (j.job_type == typ) and (j.t_fin is not None))
+    for t in JOB.types:
+        print((f"{t:>12s} " 
+               f"{m.n_arr[t]:6d} {m.n_dep[t]:6d} {m.n_arr[t]-m.n_dep[t]:6d} " 
+               f"{m.Avg_Sys_Time[t]:6.2f} "
+               f"{m.Avg_Wait_Time[t]:6.2f} "
+               f"{m.Avg_Task_Time[t]:6.2f} "
+               f"{m.Avg_Num_in_Sys[t]:6.2f}"))
 
-        n = 0
-        AST, AWT, ATT, NiS = 0, 0, 0, 0
-        for j in SIM.Arrivals:
-            if (j.job_type == typ):
-                NiS += (j.t_fin - j.t_arr) if j.t_fin else (T - j.t_arr)
-            if (j.job_type == typ) and (j.t_fin is not None):
-                n += 1
-                AST += j.t_fin - j.t_arr
-                for tt in j.task:
-                    if "S" in tt.job_type:
-                        AWT += tt.t_s_acq - tt.t_s_req
-                        AWR[typ][tt.res_grp] += tt.t_s_acq - tt.t_s_req
-                for tt in j.task:
-                    if "D" in tt.job_type:
-                        ATT += tt.t_d_fin - tt.t_d_sta
-        n = 1 if n == 0 else n
-        AST = AST / n
-        AWT = AWT / n
-        ATT = ATT / n
+    print(("\n" f"{' ':>12s} {'Util':>6s}"))
+    for rt in SHOP.ResourceTypes.keys():
+        print((f"{rt:>12} "
+               f"{100*m.Utilization[rt]:5.1f}%"))
+
+    print(("\n" f"{' ':>12s} ") + \
+          " ".join(f"{rg[:6]:>6s}" for rg in SHOP.ResourceGroups.keys()))
+    for t in JOB.types:
+        print(f"{t:>12}", end=" ")
         for rg in SHOP.ResourceGroups.keys():
-            AWR[typ][rg] /= n
-        NiS = NiS / T
-
-        print((f"{typ:>12s} {na:6d} {nc:6d} {na-nc:6d} " 
-               f"{AST:6.2f} {AWT:6.2f} {ATT:6.2f} {NiS:6.2f}"))
-    print()
-
-    print(f"{' ':>12s} " + " ".join(f"{rg[:6]:>6s}" for rg in SHOP.ResourceGroups.keys()))
-    for typ in JOB.types:
-        print(f"{typ:>12}", end=" ")
-        for rg in SHOP.ResourceGroups.keys():
-            print(f"{AWR[typ][rg]:6.2f}", end=" ")
+            print(f"{m.Avg_Wait_Res[t][rg]:6.2f}", end=" ")
         print()
     print()
 
-    print(f"{' ':>12s} {'Util':>6s}")
-    for Typ,RL in SHOP.ResourceTypes.items():
-        Utl = sum(SIM.Resource[R].occupancy.mean() for R in RL) / len(RL)
-        print(f"{Typ:>12} {100*Utl:5.1f}%")
-    print()
 
-    print(f"{'System':>12s}")
-    print(f"{'Len of Stay':>12} {SIM.System.length_of_stay.mean():6.2f}")
-    print(f"{'Num in Sys':>12} {SIM.System.length.mean():6.2f}")
 
-    return
+def Calculate_Metrics(T, W=0):
+    o = SNS()
+    o.n_arr          = {typ:0 for typ in JOB.types}
+    o.n_dep          = {typ:0 for typ in JOB.types}
+    o.n_sys          = {typ:0 for typ in JOB.types}
+    o.Utilization    = {}
+    o.Avg_Sys_Time   = {typ:0 for typ in JOB.types}
+    o.Avg_Wait_Time  = {typ:0 for typ in JOB.types}
+    o.Avg_Task_Time  = {typ:0 for typ in JOB.types}
+    o.Avg_Num_in_Sys = {typ:0 for typ in JOB.types}
+    o.Avg_Wait_Res   = {typ:{r:0 for r in SHOP.ResourceGroups.keys()} for typ in JOB.types}
+
+
+    for typ, Res in SHOP.ResourceTypes.items():
+        o.Utilization[typ] = sum(SIM.Resource[R].occupancy.mean() 
+                                 for R in Res) / len(Res)
+    
+    for j in SIM.Arrivals:
+        o.n_arr[j.job_type] += 1
+
+        if j.t_dep < inf:
+            o.n_dep[j.job_type] += 1
+
+        if W < j.t_dep:
+            o.Avg_Num_in_Sys[j.job_type] += min(T, j.t_dep) - max(W, j.t_arr)
+
+        if W < j.t_dep < inf:
+            o.Avg_Sys_Time[j.job_type] += j.t_dep - j.t_arr
+            o.n_sys[j.job_type] += 1
+
+        if W < j.t_dep < inf:
+            for tt in j.task:
+                if 'S' in tt.job_type and tt.t_s_acq:
+                    o.Avg_Wait_Time[j.job_type] += tt.t_s_acq - tt.t_s_req
+                    o.Avg_Wait_Res[j.job_type][tt.res_grp] += tt.t_s_acq - tt.t_s_req
+                if 'D' in tt.job_type:
+                    o.Avg_Task_Time[j.job_type] += tt.t_d_fin - tt.t_d_sta
+
+    non_zero = lambda x : x if x > 0 else 1
+
+    for typ in JOB.types:
+        o.Avg_Num_in_Sys[typ] /= T - W
+        o.Avg_Sys_Time[typ]   /= non_zero(o.n_sys[typ])
+        o.Avg_Wait_Time[typ]  /= non_zero(o.n_sys[typ])
+        o.Avg_Task_Time[typ]  /= non_zero(o.n_sys[typ])
+
+    for typ in JOB.types:
+        for rtyp, Res in SHOP.ResourceGroups.items():
+            o.Avg_Wait_Res[typ][rtyp] /= non_zero(o.n_sys[typ])
+
+    return o
+
